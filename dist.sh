7be80c9e73bdcb0646dfd8b3971b424c7f4915cc
T.G <farmer1992@gmail.com>
2012-12-04 21:48:38 +0800

    dish add crx

--- dist.sh
+++ dist.sh
@@ -1,7 +1,37 @@
+#/bin/bash
+
 VER=`grep '"version".*:.*".*"' manifest.json -P -o | sed -e 's/["| ]//g' | cut -d : -f 2`
-TARGET=chrome-hostadmin-$VER.zip
+TARGET=chrome-hostadmin-$VER
 
-rm -f $TARGET
-zip $TARGET hostadmin.plugin/ CodeMirror/ bootstrap/ *.js *.html *.so *.dll *.json *.png -r
+# clean
+rm -f $TARGET.zip
+rm -f $TARGET.crx
 
-echo $TARGET "DONE"
+# build for store
+zip $TARGET.zip hostadmin.plugin/ CodeMirror/ bootstrap/ *.js *.html *.so *.dll *.json *.png -r
+
+echo $TARGET.zip "DONE"
+
+if [ "$1" != "crx" ];then
+	exit
+fi
+
+# build crx useless ....
+TMP=`mktemp -d`
+PLUGINDIR="$TMP/plugin"
+mkdir -p $PLUGINDIR
+
+unzip -d $PLUGINDIR/$TARGET $TARGET.zip
+
+CHROME="/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
+
+if [ ! -e "$CHROME" ]
+then
+	CHROME='google-chrome' # ubuntu
+fi
+
+"$CHROME" --pack-extension=$PLUGINDIR/$TARGET
+
+cp $PLUGINDIR/$TARGET.crx .
+
+echo $TARGET.crx "DONE"
