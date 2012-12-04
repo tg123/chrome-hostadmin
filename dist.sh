4ea7d4c88c2dffe0cbfb9f4bcc930c47f0b9e595
T.G <farmer1992@gmail.com>
2012-12-04 21:05:56 +0800

    update dist

--- dist.sh
+++ dist.sh
@@ -1,1 +1,7 @@
-zip hostadmin.zip hostadmin.plugin/ *.js *.html *.so *.dll *.json *.png -r
+VER=`grep '"version".*:.*".*"' manifest.json -P -o | sed -e 's/["| ]//g' | cut -d : -f 2`
+TARGET=chrome-hostadmin-$VER.zip
+
+rm -f $TARGET
+zip $TARGET hostadmin.plugin/ CodeMirror/ bootstrap/ *.js *.html *.so *.dll *.json *.png -r
+
+echo $TARGET "DONE"
