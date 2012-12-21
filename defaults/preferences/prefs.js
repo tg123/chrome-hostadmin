52ce2ce1c6d82186cdcd288691d8a989ef0c8c9d
T.G <farmer1992@gmail.com>
2012-12-22 04:06:27 +0800

    restore ff perf

---
+++ defaults/preferences/prefs.js
@@ -0,0 +1,6 @@
+pref("extensions.hostadmin.hostsfilepath", "default");
+pref("extensions.hostadmin.charset", "auto");
+pref("extensions.hostadmin.firstrun", true);
+
+// https://developer.mozilla.org/en/Localizing_extension_descriptions
+pref("extensions.{bd54afa8-b14a-4d7a-aecf-37e34e882796}.description", "chrome://hostadmin/locale/overlay.properties");
